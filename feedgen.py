70918e22def4767a426627650aa98b1fe2f6e3a2
Bruce Guenter <bruce@untroubled.org>
2004-12-03 21:11:58 +0000

    Moved the XML templates into stand-alone files for easier customization.

--- feedgen.py
+++ feedgen.py
@@ -3,82 +3,26 @@ from globalfns import *
 import time
 
 class Feed:
-	def __init__(self, ctype, header, entry, footer):
+	def __init__(self, prefix, ctype):
 		self.ctype = ctype
-		self.header = header
-		self.entry = entry
-		self.footer = footer
+		self.prefix = prefix
 
 	def generate(self, ctxt, msgs):
+		header = xml(self.prefix + '-head')
+		entry = xml(self.prefix + '-item')
+		footer = xml(self.prefix + '-tail')
 		ctxt['timestamp'] = msgs[0][TIMESTAMP]
 		write("Content-Type: %s\r\n\r\n" % self.ctype)
-		write(self.header % ctxt)
+		write(header % ctxt)
 		ctxt.push()
 		for msg in msgs:
 			ctxt.update(msg)
-			write(self.entry % ctxt)
+			write(entry % ctxt)
 		ctxt.pop()
-		write(self.footer % ctxt)
+		write(footer % ctxt)
 
-atom = Feed('application/atom+xml',
-'''<?xml version="1.0" encoding="iso-8859-1"?>
-<feed version="0.3"
-      xmlns="http://purl.org/atom/ns#">
- <title>%(html(listdesc))s</title>
- <link rel="alternate"
-       type="text/html"
-       href="%(html(absurl(list=list)))s"/>
- <modified>%(isogmtime(timestamp))s</modified>
- <author>
-  <name>%(html(listemail))s</name>
- </author>
- <generator url="http://untroubled.org/ezmlm-browse/"
-            version="%(version.version)s">
-  %(version.full)s
- </generator>
-''',
-''' <entry>
-  <title>%(html(subject))s</title>
-  <link rel="alternate"
-        type="text/html"
-        href="%(html(absurl(list=list,cmd='showmsg',msgnum=msgnum)))s"/>
-  <id>%(html(absurl(list=list,cmd='showmsg',msgnum=msgnum)))s</id>
-  <author>
-   <name>%(html(author))s</name>
-  </author>
-  <issued>%(isogmtime(timestamp))s</issued>
-  <modified>%(isogmtime(timestamp))s</modified>
-  <content><pre>From: %(html(author))s
-
-%(html(mask_email(body)))s</pre></content>
- </entry>
-''',
-'''</feed>
-''')
-
-rss2 = Feed('application/rss+xml',
-'''<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0">
- <channel>
-  <title>%(html(listdesc))s</title>
-  <link>%(html(absurl(list=list)))s</link>
-  <description>%(html(listdesc))s</description>
-  <lastBuildDate>%(rfc822gmtime(timestamp))s</lastBuildDate>
-  <generator>%(version.full)s</generator>
-''',
-'''  <item>
-   <title>%(html(subject))s</title>
-   <link>%(html(absurl(list=list,cmd='showmsg',msgnum=msgnum)))s</link>
-   <pubDate>%(rfc822gmtime(timestamp))s</pubDate>
-   <description>&lt;pre&gt;From: %(html(author))s
-
-%(html(mask_email(body)))s&lt;/pre&gt;</description>
-   <guid>%(html(absurl(list=list,cmd='showmsg',msgnum=msgnum)))s</guid>
-  </item>
-''',
-''' </channel>
-</rss>
-''')
+atom = Feed('atom', 'application/atom+xml')
+rss2 = Feed('rss2', 'application/rss+xml')
 
 types = {
 	'atom': atom,
