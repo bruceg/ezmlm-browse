3d149a630dd7f18e3b31f89415ec56eee5c991dc
Bruce Guenter <bruce@untroubled.org>
2004-12-03 16:06:06 +0000

    Make sure subjects and author names are also HTML escaped.

--- feedgen.py
+++ feedgen.py
@@ -24,13 +24,13 @@ atom = Feed('application/atom+xml',
 '''<?xml version="1.0" encoding="iso-8859-1"?>
 <feed version="0.3"
       xmlns="http://purl.org/atom/ns#">
- <title>%(listdesc)s</title>
+ <title>%(html(listdesc))s</title>
  <link rel="alternate"
        type="text/html"
        href="%(html(absurl(list=list)))s"/>
  <modified>%(isogmtime(timestamp))s</modified>
  <author>
-  <name>%(listemail)s</name>
+  <name>%(html(listemail))s</name>
  </author>
  <generator url="http://untroubled.org/ezmlm-browse/"
             version="%(version.version)s">
@@ -38,13 +38,13 @@ atom = Feed('application/atom+xml',
  </generator>
 ''',
 ''' <entry>
-  <title>%(subject)s</title>
+  <title>%(html(subject))s</title>
   <link rel="alternate"
         type="text/html"
         href="%(html(absurl(list=list,cmd='showmsg',msgnum=msgnum)))s"/>
   <id>%(html(absurl(list=list,cmd='showmsg',msgnum=msgnum)))s</id>
   <author>
-   <name>%(author)s</name>
+   <name>%(html(author))s</name>
   </author>
   <issued>%(isogmtime(timestamp))s</issued>
   <modified>%(isogmtime(timestamp))s</modified>
@@ -57,17 +57,17 @@ rss2 = Feed('application/rss+xml',
 '''<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0">
  <channel>
-  <title>%(listdesc)s</title>
+  <title>%(html(listdesc))s</title>
   <link>%(html(absurl(list=list)))s</link>
-  <description>%(listdesc)s</description>
+  <description>%(html(listdesc))s</description>
   <lastBuildDate>%(rfc822gmtime(timestamp))s</lastBuildDate>
   <generator>%(version.full)s</generator>
 ''',
 '''  <item>
-   <title>%(subject)s</title>
+   <title>%(html(subject))s</title>
    <link>%(html(absurl(list=list,cmd='showmsg',msgnum=msgnum)))s</link>
    <pubDate>%(rfc822gmtime(timestamp))s</pubDate>
-   <description>From: %(author)s</description>
+   <description>From: %(html(author))s</description>
    <guid>%(html(absurl(list=list,cmd='showmsg',msgnum=msgnum)))s</guid>
   </item>
 ''',
