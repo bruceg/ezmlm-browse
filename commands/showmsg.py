aca75d1cef6dde4c94287f8538e385331e316f31
Bruce Guenter <bruce@untroubled.org>
2010-11-22 10:06:36 -0600

    commands/showmsg: Set a default value for 'threadidx'

--- commands/showmsg.py
+++ commands/showmsg.py
@@ -8,6 +8,7 @@ def do(ctxt):
 	format_timestamp(ctxt, ctxt)
 	header(ctxt, ctxt[SUBJECT], 'msg')
 	ctxt['threadnum'] = 0
+	ctxt['threadidx'] = 0
 	for i in range(len(ctxt[MESSAGES])):
 		if int(ctxt[MESSAGES][i][MSGNUM]) == ctxt[MSGNUM]:
 			ctxt['threadidx'] = i
